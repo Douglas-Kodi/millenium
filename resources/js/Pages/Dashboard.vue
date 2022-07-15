186c265ce1e1027d6454e4fbd30dc19218990626
Douglas Kodi <kodi.shimoura@gmail.com>
2022-07-15 16:09:09 -0400

    Add publications and events

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,7 +8,7 @@ import { Head } from '@inertiajs/inertia-vue3';
 
     <BreezeAuthenticatedLayout>
         <Navbar />
-        <div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
+        <div class="w3-container w3-content" style="max-width:1400px;margin-top:40px">    
             <div class="w3-row">
 
                 <!-- Left Column --> 
@@ -19,11 +19,14 @@ import { Head } from '@inertiajs/inertia-vue3';
                 </div>
                 <!-- Middle Column -->
                 <div class="w3-col m7">
-
+                    <Publication />
+                    <Post />
                 </div>
                 <!-- Right Column -->
                 <div class="w3-col m2">
-                
+                    <Events />
+                    <Solicitation />
+                    <ADS />
                 </div>
 
             </div>
@@ -38,6 +41,11 @@ import Navbar from '../Components/w3css/Navbar.vue'
 import Profile from '../Components/w3css/Profile.vue'
 import Accordion from '../Components/w3css/Accordion.vue'
 import Interests from '../Components/w3css/Interests.vue'
+import Publication from '../Components/w3css/Publication.vue'
+import Post from '../Components/w3css/Post.vue'
+import Events from '../Components/w3css/Events.vue'
+import Solicitation from '../Components/w3css/Solicitation.vue'
+import ADS from '../Components/w3css/ADS.vue'
 
 export default {
     name:"Dashboard",
@@ -45,7 +53,12 @@ export default {
         Navbar,
         Profile,
         Accordion,
-        Interests
+        Interests,
+        Publication,
+        Post,
+        Events,
+        Solicitation,
+        ADS
     }
 }
 </script>
